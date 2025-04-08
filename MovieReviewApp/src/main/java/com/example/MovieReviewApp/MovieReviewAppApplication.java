f98463153c710c0a8e715a95bda338694fe247a9
3030927 <3030927@30309-27>
2025-04-08 12:57:25 +0900

    20250408

--- MovieReviewApp/src/main/java/com/example/MovieReviewApp/MovieReviewAppApplication.java
+++ MovieReviewApp/src/main/java/com/example/MovieReviewApp/MovieReviewAppApplication.java
@@ -12,54 +12,5 @@ public class MovieReviewAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MovieReviewAppApplication.class, args);
 	}
-	
-	/*//DI
-	private final MovieService service;
-	
-	//exe
-	public void exe() {
-		//全件検索
-		System.out.println("＝＝＝＝＝＝＝＝全件検索＝＝＝＝＝＝＝＝");
-		for (Movie row : service.findAllMovie()) {
-			System.out.println(row);
-		}
-		//1件検索
-		System.out.println("＝＝＝＝＝＝＝＝1件検索＝＝＝＝＝＝＝＝");
-		System.out.println(service.findByIdMovie(1));
-		
-		//登録
-		Movie movie = new Movie();
-		movie.setTitle("サービスのテスト");
-		movie.setGenre("サービスのテスト");
-		movie.setReleaseYear(2025);
-		movie.setRating(3);
-		movie.setSummary("てすとてすとてすと");
-		movie.setReview("test/test/test/test/test/test/test/test/test");
-		movie.setImage("http://www.test.jp/test.jpg");
-		service.insertMovie(movie);
-		System.out.println("＝＝＝＝＝＝＝＝登録確認＝＝＝＝＝＝＝＝");
-		System.out.println(service.findByIdMovie(4));
-		
-		//更新
-		Movie target = service.findByIdMovie(4);
-		target.setTitle("登録のテスト：更新");
-		target.setGenre("登録のテスト：更新");
-		target.setReleaseYear(2025999);
-		target.setRating(1);
-		target.setSummary("てすとてすとてすと：更新");
-		target.setReview("test/test/test/test/test/test/test/test/test：更新");
-		target.setImage("http://www.test.jp/test.jpg：更新");
-		service.updateMovie(target);
-		System.out.println("＝＝＝＝＝＝＝＝更新確認＝＝＝＝＝＝＝＝");
-		System.out.println(service.findByIdMovie(4));
-		
-		//削除
-		service.deleteMovie(4);
-		System.out.println("＝＝＝＝＝＝＝＝削除確認＝＝＝＝＝＝＝＝");
-		//全件表示して、テスト追加した４件目が消えてるか確認
-		for (Movie row : service.findAllMovie()) {
-			System.out.println(row);
-		}
-	}*/
 
 }
--- MovieReviewApp/src/main/resources/static/js/script.js
+++ MovieReviewApp/src/main/resources/static/js/script.js
@@ -1,80 +1,61 @@
-$(function(){
-	// ページ全体1秒かけてフェードイン
-	$(document).ready(function () {
-	  $("body").fadeIn(750);
-	});
+$(function () {
 	
-	//* 画像のフェードアップ
-	$(window).on('load',function(){
-	
-    $(window).scroll(function (){
-        $('.fade-up').each(function(){
-            var POS = $(this).offset().top;
-            var scroll = $(window).scrollTop();
-            var windowHeight = $(window).height();
+  // ページ全体0.75秒かけてフェードイン
+  $(document).ready(function () {
+    $("body").fadeIn(750);
+  });
 
-            if (scroll > POS - windowHeight){
-                $(this).css({
-                        'opacity':'1',
-                        'transform':'translateY(0)',
-                        '-webkit-transform':'translateY(0)',
-                        '-moz-transform':'translateY(0)',
-                        '-ms-transform':'translateY(0)'
-                });
-            } else {
-                $(this).css({
-                        'opacity':'0',
-                        'transform':'translateY(70px)',
-                        '-webkit-transform':'translateY(70px)',
-                        '-moz-transform':'translateY(70px)',
-                        '-ms-transform':'translateY(70px)'
-						});
-					}
-				});
-			});
-		});
+  //* 画像のフェードアップ
+  $(window).on('load', function () {
+    $(window).scroll(function () {
+      $('.fade-up').each(function () {
+        var POS = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
 
-	//* トップへ戻る
-	$(document).ready(function () {
-	    var $button = $("#scrollTopBtn");
+        if (scroll > POS - windowHeight) {
+          $(this).css({
+            'opacity': '1',
+            'transform': 'translateY(0)',
+            '-webkit-transform': 'translateY(0)',
+            '-moz-transform': 'translateY(0)',
+            '-ms-transform': 'translateY(0)'
+          });
+        } else {
+          $(this).css({
+            'opacity': '0',
+            'transform': 'translateY(70px)',
+            '-webkit-transform': 'translateY(70px)',
+            '-moz-transform': 'translateY(70px)',
+            '-ms-transform': 'translateY(70px)'
+          });
+        }
+      });
+    });
+  });
 
-	    // スクロール位置を監視してボタンを表示・非表示
-	    $(window).scroll(function () {
-	        if ($(this).scrollTop() > 500) {
-	            $button.fadeIn();
-	        } else {
-	            $button.fadeOut();
-	        }
-	    });
+  //* トップへ戻る
+  $(document).ready(function () {
+    var $button = $("#scrollTopBtn");
 
-	    // スムーズにスクロールして最上部へ移動
-	    $button.click(function () {
-	        $("html, body").animate({ scrollTop: 0 }, 500); // 500ms かけてスクロール
-	    });
-	});
-	
-	// ソートフォーム送信時にスクロール位置をURLに追加
-	    $(document).ready(function() {
-	        $('form').submit(function(event) {
-	            var scrollPosition = $(window).scrollTop(); // 現在のスクロール位置を取得
-	            var sortValue = $('#sort').val(); // 現在選ばれているソート値を取得
-	            var currentUrl = window.location.href.split('?')[0]; // 現在のURLからクエリを削除
-	            
-	            if (scrollPosition > 0 || sortValue) {
-	                // URLにスクロール位置とソート情報を追加
-	                window.location.href = currentUrl + '?scroll=' + scrollPosition + '&sort=' + sortValue;
-	            } else {
-	                // 通常のフォーム送信
-	                this.submit();
-	            }
-	        });
+    // スクロール位置を監視してボタンを表示・非表示
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 500) {
+        $button.fadeIn();
+      } else {
+        $button.fadeOut();
+      }
+    });
 
-	        // ページが読み込まれたときにスクロール位置を復元
-	        var scrollPosition = new URLSearchParams(window.location.search).get('scroll');
-	        if (scrollPosition) {
-	            $(window).scrollTop(scrollPosition); // スクロール位置を復元
-	        }
-	    });
-	
-	
+    // スムーズにスクロールして最上部へ移動
+    $button.click(function () {
+      $("html, body").animate({ scrollTop: 0 }, 500);
+    });
+  });
+  
+
+  // ソート切り替え時にふわっと表示される
+  $(".cell").each(function(index) {
+    $(this).css("opacity", "0").delay(100 * index).animate({ opacity: 1 }, 500);
+  });
 });
